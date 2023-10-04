38a3230dcc69ee03b06b0ec42c12c2134ec7c356
chinez <47869879+chinezprost@users.noreply.github.com>
2023-10-04 19:03:57 +0300

    Delete laborator1_rust/target directory

--- laborator1_rust/target/debug/deps/laborator1_rust-d27793e042557ab2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\plays\lab_1\RUST-Nita-Dennis-Alexandru-2E2\laborator1_rust\target\debug\deps\laborator1_rust-d27793e042557ab2.rmeta: src\main.rs
-
-C:\Users\plays\lab_1\RUST-Nita-Dennis-Alexandru-2E2\laborator1_rust\target\debug\deps\laborator1_rust-d27793e042557ab2.d: src\main.rs
-
-src\main.rs:
--- laborator1_rust/target/debug/deps/laborator1_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\plays\lab_1\RUST-Nita-Dennis-Alexandru-2E2\laborator1_rust\target\debug\deps\laborator1_rust.exe: src\main.rs
-
-C:\Users\plays\lab_1\RUST-Nita-Dennis-Alexandru-2E2\laborator1_rust\target\debug\deps\laborator1_rust.d: src\main.rs
-
-src\main.rs:
